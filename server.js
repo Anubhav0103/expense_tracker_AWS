c4b9d1ab00e080ec621c8c416438b027493e4745
Anubhav <anubhav06cd@gmail.com>
2025-04-27 21:12:47 +0530

    Add /health endpoint and update health check path

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const helmet = require('helmet');
 
 const app = express();
 
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+  });
+
 // Helmet with CSP
 app.use(helmet({
     contentSecurityPolicy: {
